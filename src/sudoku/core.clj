0801cf91cdb54cd7a36decb74d6ce604c4bebf6f
dajanka <dajana.stiberova@gmail.com>
2015-01-14 12:13:43 +0100

    Function for removing numbers from grid and created simple sudoku solver

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -1,19 +1,13 @@
 (ns sudoku.core
-  (:require [clojure.pprint :refer :all]))
+  (:require [clojure.set :as set]
+            [clojure.pprint :refer :all]))
 
-(def board
-  {[0 0] (into [] (map (partial into [])
-                       (partition 3 (shuffle (range 1 10)))))})
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; UTILS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(comment
-  {[0 0] [[9 2 4]
-          [6 1 3]
-          [5 8 7]]})
-
-(defn one-nine-rand-without [& exclusions]
+(defn zero-eight-rand-without [& exclusions]
   (let [random-ranges (->> exclusions
-                           (cons 0)
-                           (cons 10)
+                           (cons -1)
+                           (cons 9)
                            set
                            sort
                            (partition 2 1)
@@ -23,9 +17,11 @@
     (+ (rand-int (- (dec upper) lower))
        (inc lower))))
 
-(defn prime []
-  (assoc board [0 1]
-         ()))
+(defn transpose [matrix]
+  (mapv (fn [idx]
+          (mapv #(nth % idx) matrix))
+        (range 0 (count matrix))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; NEW SUDOKU ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def start-board
@@ -71,9 +67,70 @@
                  (map (fn [section]
                         (reduce into {} section)))))))
 
-(defn final-board []
-  (->> start-board
+(defn final-board [board]
+  (->> board
        swap-board
        rows->columns
        swap-board
-       rows->columns))
+       rows->columns
+       vals
+       (mapcat vals)
+       (into [])))
+
+;;;;;;;;;;;;;;;;;;;;;;;; REMOVE NUMBERS ;;;;;;;;;;;;;;;;;;;;;
+
+(defn remove-number [board]
+  (let [board board
+        row (rand-int 9)
+        second-row (- 8 row)
+        index (rand-int 9)
+        second-index (- 8 index)]
+    (-> board
+        (assoc-in [row index] nil)
+        (assoc-in [second-row second-index] nil))))
+
+(defn remove-numbers [board]
+  (loop [i 0
+         b board]
+    (if (= i 19)
+      b
+      (recur (inc i)
+             (remove-number b)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;; FINAL GENERATE ;;;;;;;;;;;;;;;;;;;;;
+
+(def generate (comp remove-numbers final-board))
+
+;;;;;;;;;;;;;;;;;;;;;;;; SOLVER ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def ^:private full-set #{1 2 3 4 5 6 7 8 9})
+
+(defn candidates [series]
+  (-> series set (disj nil) (as-> row-set (set/difference full-set row-set))))
+
+(defn choose-candidate [row column]
+  (let [final-candidates (set/intersection (candidates row)
+                                           (candidates column))]
+    (when (= 1 (count final-candidates))
+      (first final-candidates))))
+
+(defn solver-pass [board]
+  (let [colls (transpose board)]
+    (mapv (fn [row]
+            (mapv (fn [number idx]
+                    (if number
+                      number
+                      (choose-candidate row (nth colls idx))))
+                  row (range 0 9)))
+          board)))
+
+
+(def hard-board [[nil 9 nil 3 nil 1 nil 4 nil nil]
+                 [nil 1 nil nil 4 nil nil nil nil]
+                 [2 4 nil nil 9 7 nil 1 nil]
+                 [7 8 9 nil 1 nil nil nil nil]
+                 [4 2 nil nil nil nil nil nil 1]
+                 [nil 3 1 4 nil 2 7 nil 9]
+                 [1 nil 8 nil nil nil 3 nil nil]
+                 [3 nil 4 1 nil nil nil 5 2]
+                 [9 nil 2 nil nil nil 1 7 nil]])
